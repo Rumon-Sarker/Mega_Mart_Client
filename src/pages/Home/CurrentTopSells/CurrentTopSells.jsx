53690b4ad782d4595239d320700163a6ad97d450
Rumon-Sarker <mdsrrumon1234@gmail.com>
2024-10-26 22:36:14 +0600

    current Top sells Responsive

--- src/pages/Home/CurrentTopSells/CurrentTopSells.jsx
+++ src/pages/Home/CurrentTopSells/CurrentTopSells.jsx
@@ -39,6 +39,33 @@ const CurrentTopSells = () => {
                     spaceBetween={30}
                     slidesPerView={6}
                     loop={true}
+                    breakpoints={{
+                        // when window width is >= 320px
+                        320: {
+                            slidesPerView: 1,
+                            spaceBetween: 10,
+                        },
+                        // when window width is >= 480px
+                        480: {
+                            slidesPerView: 2,
+                            spaceBetween: 20,
+                        },
+                        // when window width is >= 768px
+                        768: {
+                            slidesPerView: 2,
+                            spaceBetween: 30,
+                        },
+                        // when window width is >= 1024px
+                        1024: {
+                            slidesPerView: 4,
+                            spaceBetween: 30,
+                        },
+                        // when window width is >= 1280px
+                        1280: {
+                            slidesPerView: 5,
+                            spaceBetween: 30,
+                        },
+                    }}
 
                     pagination={{ clickable: true }}
 
@@ -58,13 +85,13 @@ const CurrentTopSells = () => {
                 </Swiper>
             </div>
 
-            <div className='flex gap-12 my-12'>
+            <div className='flex-none gap-12 my-12 space-y-3 xl:space-y-0 lgl:space-y-0 lg:flex lgl:flex'>
                 <div className='w-full h-[250px] rounded-lg bg-gradient-to-r from-purple-500 to-pink-500 p-12 shadow-lg'>
-                    <div className="w-full h-[200px]   transform transition hover:scale-105 duration-700 rounded-lg  flex  items-center">
+                    <div className="w-full xl:h-[200px] lgl:h-[160px]   transform transition hover:scale-105 duration-700 rounded-lg flex items-center">
                         {/* Left Side: Product Information */}
-                        <div className="flex-1 space-y-3 text-white">
-                            <h2 className="text-4xl font-bold">World Fist HD LED Tv</h2>
-                            <p className="text-2xl font-semibold">First Order and Take this offer.Not miss this offers</p>
+                        <div className="flex-1 space-y-2 text-white">
+                            <h2 className="text-xl font-bold xl:text-4xl lgl:text-2xl lg:text-2xl mdl:text-2xl md:text-2xl">World Fist HD LED Tv</h2>
+                            <p className="font-semibold xl:text-2xl lgl:text-xl md:text-xl">First Order and Take this offer.Not miss this offers</p>
                             <button className="px-6 py-4 font-semibold transition rounded-full bg-gradient-to-r from-purple-700 to-pink-500 hover:bg-purple-100">
                                 Shop Now
                             </button>
@@ -72,7 +99,7 @@ const CurrentTopSells = () => {
 
                         {/* Right Side: Product Image with same gradient background */}
                         <div className="flex-shrink-0 rounded-lg">
-                            <div className="relative w-[250px] h-[200px] rounded-lg">
+                            <div className="relative xl:w-[250px] lg:-[200px] w-[180px] h-[200px] rounded-lg">
                                 <img
                                     src={ledTv} // Replace with your PNG image URL
                                     alt="ledTv"
@@ -83,11 +110,11 @@ const CurrentTopSells = () => {
                     </div>
                 </div>
                 <div className='w-full h-[250px] rounded-lg bg-gradient-to-r from-yellow-500 to-pink-500 p-12 shadow-lg'>
-                    <div className="w-full h-[200px]   transform transition hover:scale-105 duration-700 rounded-lg  flex  items-center">
+                    <div className="w-full xl:h-[200px] lgl:h-[160px]   transform transition hover:scale-105 duration-700 rounded-lg  flex  items-center">
                         {/* Left Side: Product Information */}
-                        <div className="flex-1 space-y-3 text-white">
-                            <h2 className="text-4xl font-bold">Rewords Programs</h2>
-                            <p className="text-xl font-semibold">Welcome to megamart and Take to Your Rewords</p>
+                        <div className="flex-1 space-y-2 text-white">
+                            <h2 className="text-xl font-bold xl:text-4xl lgl:text-3xl lg:text-3xl md:text-2xl">Rewords Programs</h2>
+                            <p className="font-semibold xl:text-2xl lgl:text-xl md:text-xl">Welcome to megamart and Take to Your Rewords</p>
                             <button className="px-6 py-4 font-semibold transition rounded-full bg-gradient-to-r from-yellow-500 to-pink-500 hover:bg-purple-100">
                                 Shop Now
                             </button>
@@ -95,7 +122,7 @@ const CurrentTopSells = () => {
 
                         {/* Right Side: Product Image with same gradient background */}
                         <div className="flex-shrink-0 rounded-lg">
-                            <div className="relative w-[250px] h-[200px] rounded-lg">
+                            <div className="relative xl:w-[250px] lgl:w-[200px] lg:-[200px] w-[180px] h-[200px] rounded-lg">
                                 <img
                                     src={rewordImg} // Replace with your PNG image URL
                                     alt="rewordImg"
